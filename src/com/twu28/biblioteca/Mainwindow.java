1d14b509bad9726187c1f7223b3afd6e4f241e07
rohit# <roh422@yahoo.com>
2012-07-14 16:36:52 +0530

    Welcome page

---
+++ src/com/twu28/biblioteca/Mainwindow.java
@@ -0,0 +1,17 @@
+package com.twu28.biblioteca;
+
+import org.junit.internal.matchers.StringContains;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: HP
+ * Date: 12/7/12
+ * Time: 8:34 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Mainwindow {
+    public static void main(String[] args){
+
+        System.out.println("this is new java");
+    }
+}
